8863ae524d3f4ad48318b6b07ce98e1d456a0ad8
kaervek <barteksiewniak@gmail.com>
2017-12-26 17:32:37 +0100

    Added apache commons lib, working towards refactoring and copy file implementation

--- src/main/java/filemanager/controllers/MainController.java
+++ src/main/java/filemanager/controllers/MainController.java
@@ -21,6 +21,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,21 +29,26 @@ import java.io.IOException;
 
 public class MainController {
     @FXML
-    private Label hddSpaceInfo;
+    private Label hddSpaceInfoLeft;
     @FXML
-    private Label currentPathLeftDisplayLabel;
+    private Label hddSpaceInfoRight;
+    @FXML
+    private Label currentActiveDisplayPath;
     @FXML
     private TableView<FileModel> leftDisplay;
     @FXML
     private TableView<FileModel> rightDisplay;
     @FXML
-    private ComboBox<String> driveSelect;
+    private ComboBox<String> driveSelectLeft;
+    @FXML
+    private ComboBox<String> driveSelectRight;
 
     private FileAndFolderGatherer fileAndFolderGatherer;
     private ApplicationProperties properties;
     private FileToModelListConverter converter;
     private FocusDisplay focusedDisplay;
-    private String currentFolder;
+    private String selectedLeftDisplayDir;
+    private String selectedRightDisplayDir;
     private StageManager stageManager;
 
     @FXML
@@ -56,17 +62,27 @@ public class MainController {
     }
 
     private void selectDriveListener() {
-        driveSelect.valueProperty().addListener(new ChangeListener<String>() {
-            @Override public void changed(ObservableValue ov, String oldValue, String newValue) {
-                if (newValue.equals("C:\\")) {
-                    currentFolder = "C:\\";
-                    currentPathLeftDisplayLabel.textProperty().setValue(currentFolder);
-                    fillLeftDisplayWithData("C:\\");
-                } else if (newValue.equals("D:\\")) {
-                    currentFolder = "D:\\";
-                    currentPathLeftDisplayLabel.textProperty().setValue(currentFolder);
-                    fillLeftDisplayWithData("D:\\");
-                }
+        driveSelectLeft.valueProperty().addListener((ov, oldValue, newValue) -> {
+            if (newValue.equals("C:\\")) {
+                selectedLeftDisplayDir = "C:\\";
+                currentActiveDisplayPath.textProperty().setValue(selectedLeftDisplayDir);
+                fillLeftDisplayWithData("C:\\");
+            } else if (newValue.equals("D:\\")) {
+                selectedLeftDisplayDir = "D:\\";
+                currentActiveDisplayPath.textProperty().setValue(selectedLeftDisplayDir);
+                fillLeftDisplayWithData("D:\\");
+            }
+        });
+
+        driveSelectRight.valueProperty().addListener((ov, oldValue, newValue) -> {
+            if (newValue.equals("C:\\")) {
+                selectedRightDisplayDir = "C:\\";
+                currentActiveDisplayPath.textProperty().setValue(selectedRightDisplayDir);
+                fillRightDisplayWithData("C:\\");
+            } else if (newValue.equals("D:\\")) {
+                selectedRightDisplayDir = "D:\\";
+                currentActiveDisplayPath.textProperty().setValue(selectedRightDisplayDir);
+                fillRightDisplayWithData("D:\\");
             }
         });
     }
@@ -90,7 +106,10 @@ public class MainController {
 
     private void prepareDataForHDDSpaceLabel() throws IOException {
         HDDSpaceTracker hddSpaceTracker = new HDDSpaceTracker();
-        hddSpaceInfo.textProperty().setValue
+        hddSpaceInfoLeft.textProperty().setValue
+                (hddSpaceTracker.getAmountOfSpaceFromSelectedDrive(HDDSpaceTracker.TypeForHDDSpaceAmountSelector.FREE_SPACE_AMOUNT) + " k from " +
+                        (hddSpaceTracker.getAmountOfSpaceFromSelectedDrive(HDDSpaceTracker.TypeForHDDSpaceAmountSelector.TOTAL_SPACE_AMOUNT) + " free"));
+        hddSpaceInfoRight.textProperty().setValue
                 (hddSpaceTracker.getAmountOfSpaceFromSelectedDrive(HDDSpaceTracker.TypeForHDDSpaceAmountSelector.FREE_SPACE_AMOUNT) + " k from " +
                         (hddSpaceTracker.getAmountOfSpaceFromSelectedDrive(HDDSpaceTracker.TypeForHDDSpaceAmountSelector.TOTAL_SPACE_AMOUNT) + " free"));
     }
@@ -121,6 +140,7 @@ public class MainController {
         ObservableList<FileModel> itemsForRightDisplay =
                 FXCollections.observableArrayList(converter.convert(fileAndFolderGatherer.getStructureForRootPath(path)));
         rightDisplay.setItems(itemsForRightDisplay);
+        rightDisplay.getSelectionModel().select(0);
     }
 
     @FXML
@@ -132,6 +152,10 @@ public class MainController {
         if (e.getCode() == KeyCode.F4) {
             openEditor();
         }
+
+        if (e.getCode() == KeyCode.F5) {
+            copyFile();
+        }
     }
 
     private void updateAndRefreshListOfFilesForView(TableView<FileModel> view) {
@@ -140,14 +164,23 @@ public class MainController {
         if (!path.equals("...")) {
             items = FXCollections.observableArrayList(converter.convert(fileAndFolderGatherer
                     .getStructureForRootPath(path)));
-            currentFolder = path;
-            currentPathLeftDisplayLabel.textProperty().setValue(path);
+            selectedLeftDisplayDir = path;
+            currentActiveDisplayPath.textProperty().setValue(path);
         } else {
             items = FXCollections.observableArrayList(converter.convert(fileAndFolderGatherer
-                    .getStructureForRootPath(converter.getParentPath(currentFolder))));
-            currentFolder = converter.getParentPath(currentFolder);
-            currentPathLeftDisplayLabel.textProperty().setValue(currentFolder);
+                    .getStructureForRootPath(converter.getParentPath(selectedLeftDisplayDir))));
+            selectedLeftDisplayDir = converter.getParentPath(selectedLeftDisplayDir);
+            currentActiveDisplayPath.textProperty().setValue(selectedLeftDisplayDir);
         }
+        view.setItems(items);
+        view.refresh();
+        view.getSelectionModel().select(0);
+    }
+
+    private void refresh(TableView<FileModel> view) {
+        ObservableList<FileModel> items;
+        items = FXCollections.observableArrayList(converter.convert(fileAndFolderGatherer
+                .getStructureForRootPath(selectedLeftDisplayDir)));
         view.setItems(items);
         view.refresh();
         view.getSelectionModel().select(0);
@@ -173,12 +206,14 @@ public class MainController {
         File[] drives = File.listRoots();
         if (drives != null && drives.length > 0) {
             for (File aDrive : drives) {
-                driveSelect.getItems().add(aDrive.toString());
+                driveSelectLeft.getItems().add(aDrive.toString());
+                driveSelectRight.getItems().add(aDrive.toString());
             }
         }
-        driveSelect.getSelectionModel().selectFirst();
-        currentFolder = driveSelect.getSelectionModel().selectedItemProperty().getValue();
-        currentPathLeftDisplayLabel.textProperty().setValue(currentFolder);
+        driveSelectLeft.getSelectionModel().selectFirst();
+        driveSelectRight.getSelectionModel().selectFirst();
+        selectedLeftDisplayDir = driveSelectLeft.getSelectionModel().selectedItemProperty().getValue();
+        currentActiveDisplayPath.textProperty().setValue(selectedLeftDisplayDir);
     }
 
     public void openEditor() throws IOException {
@@ -199,5 +234,15 @@ public class MainController {
             view = rightDisplay;
         }
         return view;
+    }
+
+    @FXML
+    private void copyFile() throws IOException {
+        File file = new File(getActiveView().getSelectionModel().getSelectedItem().getFile().getPath());
+        File destinationDir = new File("C:\\test-directory");
+
+        FileUtils.copyFileToDirectory(file, destinationDir);
+        refresh(leftDisplay);
+        refresh(rightDisplay);
     }
 }
